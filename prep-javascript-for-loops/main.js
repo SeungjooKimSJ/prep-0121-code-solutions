0ee6a16fb397797136d9aa36eafd7226254ad84c
Seungjoo Kim <sjskybone@gmail.com>
2021-01-16 12:59:55 -0800

    completed prep-javascript-for-loops

--- prep-javascript-for-loops/main.js
+++ prep-javascript-for-loops/main.js
@@ -1,22 +1,18 @@
 // create your loops here.
-let i;
-for (i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
   console.log(i);
 }
 
-let j;
-for (j = 0; j < 50; j++) {
+for (let j = 0; j < 50; j++) {
   if (j % 2 !== 0) {
     console.log(j);
   }
 }
 
-let k;
-for (k = 0; k < 20; k += 2) {
+for (let k = 0; k < 20; k += 2) {
   console.log(k);
 }
 
-let l;
-for (l = 100; l > 0; l--) {
+for (let l = 100; l > 0; l--) {
   console.log('Time till explosion ' + l + '!');
 }
